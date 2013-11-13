c92cc9b16b53bd271443481d07003ebd97f8c8d4
kasey lundquist <klundquist2014@kellogg.northwestern.edu>
2013-11-13 15:14:34 -0600

    Create app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rollcall::Application.config.secret_key_base = 'ebe0670341ba4f820e40e31726124a4a0c8b9bdca7341a323563c8d529e2758c5a7432a94e862e09aad5af2718da640ece0ca40f12951d0fa96a9e6261f7f780'
